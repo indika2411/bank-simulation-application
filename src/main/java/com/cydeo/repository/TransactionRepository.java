0cc144c97f1066b31a6491fc710ef180def6330f
Indira Carman <indika2411@yahoo.com>
2022-09-10 11:58:08 -0400

    app test

--- src/main/java/com/cydeo/repository/TransactionRepository.java
+++ src/main/java/com/cydeo/repository/TransactionRepository.java
@@ -1,10 +1,11 @@
 package com.cydeo.repository;
 
 import com.cydeo.model.Transaction;
+import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.List;
-
+@Component
 public class TransactionRepository {
 
     public static List<Transaction> transactionList = new ArrayList<>();
